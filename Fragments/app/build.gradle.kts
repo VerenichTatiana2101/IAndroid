e73061ecb547225353d068b31eeb1e2efffbb9b5
Verenich Tatiana <verenik91@gmail.com>
2024-04-12 08:59:08 +0300

    androidX navigation

--- Fragments/app/build.gradle.kts
+++ Fragments/app/build.gradle.kts
@@ -48,5 +48,9 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    // fragment extensions
     implementation("androidx.fragment:fragment-ktx:1.6.2")
+    // navigation
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
 }
