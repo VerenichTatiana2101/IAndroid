d884cf9a00d040539f749c13171c5f5e828c4edf
Verenich Tatiana <verenik91@gmail.com>
2024-04-14 18:50:09 +0300

    home work fragments

--- QuizzyMania/app/src/main/java/com/quizzymania/quiz/QuizResult.kt
+++ QuizzyMania/app/src/main/java/com/quizzymania/quiz/QuizResult.kt
@@ -1,6 +1,7 @@
 package com.quizzymania.quiz
 
 import android.annotation.SuppressLint
+import android.media.MediaPlayer
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -16,6 +17,7 @@ class QuizResult : Fragment() {
     private val args: QuizResultArgs by navArgs()
     private var _binding: QuizResultBinding? = null
     private val binding get() = _binding!!
+    private var mediaPlayer: MediaPlayer? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,18 +30,30 @@ class QuizResult : Fragment() {
     @SuppressLint("SetTextI18n")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        if(args.number == 3) binding.resultQuiz.text = getString(R.string.result_win)
-        else binding.resultQuiz.text = getString(R.string.result_full)
+        if (args.number == 3) {
+            binding.resultQuiz.text = getString(R.string.result_win)
+            playMelody(R.raw.right_answer)
+        } else {
+            binding.resultQuiz.text = getString(R.string.result_full)
+            playMelody(R.raw.wrong_answer)
+        }
 
-        binding.result.text = args.number.toString() + "/3"
+        binding.result.text = "${args.number}/3"
 
         binding.button.setOnClickListener {
-           findNavController().navigate(R.id.action_quizResult_to_QuizFragment)
-     }
+            findNavController().navigate(R.id.action_quizResult_to_QuizFragment)
+        }
+    }
+
+    private fun playMelody(resourceId: Int) {
+        //mediaPlayer?.release()
+        mediaPlayer = MediaPlayer.create(requireContext(), resourceId)
+        mediaPlayer?.start()
     }
 
     override fun onDestroy() {
         super.onDestroy()
+        mediaPlayer?.release()
         _binding = null
     }
 }
