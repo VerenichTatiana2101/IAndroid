adcb980e24891212f16ee1047f10634959b64320
Verenich Tatiana <verenik91@gmail.com>
2024-04-16 12:13:31 +0300

    acti

---
+++ Sources/example_8.2.4_dialog/app/src/main/java/com/example/myapplication/FirstFragment.kt
@@ -0,0 +1,42 @@
+package com.example.myapplication
+
+import android.app.Dialog
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.myapplication.databinding.FragmentFirstBinding
+
+
+class FirstFragment : Fragment() {
+
+    private var _binding: FragmentFirstBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val dialog = Dialog(requireActivity(), R.style.CustomDialog)
+        dialog.setTitle("Dialog title")
+        dialog.setContentView(R.layout.dialog_layout)
+
+        binding.showDialog.setOnClickListener {
+            dialog.show()
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
